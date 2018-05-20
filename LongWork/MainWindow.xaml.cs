7dd46177805a7700333d4a1aaf5da118ba6b95b7
Aida7 <33822250+Aida7@users.noreply.github.com>
2018-05-20 23:26:47 +0600

    work

---
+++ LongWork/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LongWork
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Thread thread;
+        public MainWindow()
+        {
+            InitializeComponent();
+        
+        }
+        public void PrintHello(object i)
+        {
+            Thread.Sleep(5000);
+            Dispatcher.Invoke(new Action(() => text.Content = "Hi!!"));
+
+        }
+
+        private void Stop_Clik(object sender, RoutedEventArgs e)
+        {
+            thread.Suspend();
+            MessageBox.Show("поток остоновлен");
+        }
+
+        private void Save_Clik(object sender, RoutedEventArgs e)
+        {
+            thread = new Thread(new ParameterizedThreadStart(PrintHello));
+            thread.Start(text);
+        
+        }
+    }
+}
